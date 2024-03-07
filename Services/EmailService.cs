b42522f0a79cc5f67373b7e381dba0117c106644
Shawn Cook <sfncook@gmail.com>
2024-03-07 14:40:32 -0800

    Rebrand emails with Greeter.bot

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -37,101 +37,101 @@ public class EmailService
         string convo_id
     ) {
             await SendEmail(
-                "hello@keli.ai", 
-                "Keli.AI",
+                "hello@greet.bot", 
+                "Greeter.Bot",
                 recipient_email, 
-                "New website lead from Keli.AI", 
+                "New website lead from Greeter.Bot", 
                 $@"
 Hello!
 
-Good news -- you have a new website lead from Keli.AI. Here is their information:
+Good news -- you have a new website lead from Greeter.Bot. Here is their information:
 
 First Name: {assistantResponse.user_first_name}
 Last Name: {assistantResponse.user_last_name}
 Email:  {assistantResponse.user_email}
 Phone: {assistantResponse.user_phone_number}
-Conversation history: https://admin.keli.ai/messages?convo_id={convo_id}
+Conversation history: https://app.greeter.bot/messages?convo_id={convo_id}
 
 Please reach out to them as soon as possible.
 
 Thank you,
-The Keli.AI team
+The Greeter.Bot team
 "
             );
         }
 
     public async Task SendRegistrationEmail(string recipient_email) {
             await SendEmail(
-                "hello@keli.ai", 
-                "Keli.AI",
+                "hello@greeter.bot", 
+                "Greeter.Bot",
                 recipient_email, 
-                "Keli.AI registration received", 
+                "Greeter.Bot registration received", 
                 @"
 Hello! 
 
-Thanks for registering for a free trial account at Keli.AI. We will approve your account as quickly as possible (usually within 24 hours) and let you know. If you haven't heard from us within a few days, feel free to reply to this email. 
+Thanks for registering for a free trial account at Greeter.Bot. We will approve your account as quickly as possible (usually within 24 hours) and let you know. If you haven't heard from us within a few days, feel free to reply to this email. 
 
 Thank you,
-The Keli.AI team
+The Greeter.Bot team
 "
             );
         }
 
     public async Task SendRegistrationApprovalEmail(string recipient_email) {
             await SendEmail(
-                "hello@keli.ai", 
-                "Keli.AI",
+                "hello@greeter.bot", 
+                "Greeter.Bot",
                 recipient_email, 
-                "Your Keli.AI registration was approved", 
+                "Your Greeter.Bot registration was approved", 
                 @"
 Hello! 
 
-Good news -- your Keli.AI registration was approved, and your account is now active. Please log in here: https://admin.keli.ai
+Good news -- your Greeter.Bot registration was approved, and your account is now active. Please log in here: https://app.greeter.bot
 
-See our Getting Started guide at https://docs.keli.ai
+See our Getting Started guide at https://docs.greeter.bot
 
 If you have any questions, feel free to reply to this email.
 
 Thank you,
-Keli.AIt team
+Greeter.Bot team
 "
             );
         }
 
         public async Task SendRegistrationDeniedEmail(string recipient_email) {
             await SendEmail(
-                "hello@keli.ai", 
-                "Keli.AI", 
+                "hello@greeter.bot", 
+                "Greeter.Bot", 
                 recipient_email, 
-                "Your Keli.AI registration was declined", 
+                "Your Greeter.Bot registration was declined", 
                 @"
 Hello! 
 
-Unfortunately, your Keli.AI registration was declined. Be sure to use a company email address when registering for an account. We don't accept personal emails, e.g. Gmail, Yahoo, Hotmail, etc. See here for more information: https://docs.keli.ai
+Unfortunately, your Greeter.Bot registration was declined. Be sure to use a company email address when registering for an account. We don't accept personal emails, e.g. Gmail, Yahoo, Hotmail, etc. See here for more information: https://docs.greeter.bot
 
 If you have any questions, feel free to reply to this email.
 
 Thank you,
-The Keli.AI team
+The Greeter.Bot team
 "
             );
         }
 
         public async Task SendNewRegistrationAdminEmail() {
             await SendEmail(
-                "hello@keli.ai", 
-                "Keli.AI", 
-                "sfncook@gmail.com,kelidotai@gmail.com", 
-                "Keli.AI: New account registration", 
+                "hello@greeter.bot", 
+                "Greeter.Bot", 
+                "james@greeter.bot,shawn@greeter.bot", 
+                "Greeter.Bot: New account registration", 
                 @"
 Hello! 
 
-A new account has been registered in the Keli.AI Admin Portal.  It is waiting for approval.
+A new account has been registered in the Greeter.Bot Admin Portal.  It is waiting for approval.
 
-https://admin.keli.ai/users
+https://app.greeter.bot/users
 
 Thank you,
-The Keli.AI team
+The Greeter.Bot team
 "
             );
         }
